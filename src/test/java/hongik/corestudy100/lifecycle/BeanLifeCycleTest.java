6fa48da292d49a5b8d570131deb8c09de4ea8cca
jeon-ji-yoon <rebegin15@gmail.com>
2021-07-08 22:39:30 +0900

    빈 생명주기 콜백 - 객체의 생성과 초기화 작업을 분리하는 것이 좋다

---
+++ src/test/java/hongik/corestudy100/lifecycle/BeanLifeCycleTest.java
@@ -0,0 +1,28 @@
+package hongik.corestudy100.lifecycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+public class BeanLifeCycleTest {
+
+    @Test
+    void lifeCycleTest(){
+        ConfigurableApplicationContext ac = new AnnotationConfigApplicationContext(LifeCycleConfig.class);
+        NetworkClient client = ac.getBean(NetworkClient.class);
+        ac.close();
+    }
+
+    @Configuration
+    static class LifeCycleConfig{
+
+        @Bean
+        public NetworkClient netWorkClient(){
+            NetworkClient networkClient = new NetworkClient();
+            networkClient.setUrl("www.hongik.ac.kr");
+            return networkClient;
+        }
+    }
+}
